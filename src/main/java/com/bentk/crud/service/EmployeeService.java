a976d9ad7c66238632a2621693246220ae86bc16
BenTK000 <bentk000@aliyun.com>
2020-12-17 22:07:09 +0800

    Initial commit

---
+++ src/main/java/com/bentk/crud/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.bentk.crud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Ben
+ * @date 2020-12-04 15:40
+ */
+@Service
+public class EmployeeService {
+//    @Autowired
+//    EmployeeMapper employeeMapper;
+//
+//    /**
+//     * 查询所有员工
+//     * @return
+//     */
+//    public List<Employee> getAll() {
+//        // TODO Auto-generated method stub
+//        return employeeMapper.selectByExampleWithDept(null);
+//    }
+}
